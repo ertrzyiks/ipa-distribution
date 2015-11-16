17082e76258dbe96feeb997565b494bbbaee4794
Mateusz Derks <mateusz.derks@schibsted.pl>
2015-11-16 16:42:19 +0100

    Change path to sqlite file

--- knexfile.js
+++ knexfile.js
@@ -11,14 +11,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/dev.sqlite3'
     }
   },
 
   production: {
     client: 'sqlite3',
     connection: {
-      filename: './ipa-distribution.sqlite3'
+      filename: './data/ipa-distribution.sqlite3'
     }
   }
 };
